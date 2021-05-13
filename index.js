af84b9793130769316c6c1ecdf4cacc5f3f0277a
ShreeyaSaini <74184402+ShreeyaSaini@users.noreply.github.com>
2021-05-13 23:46:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+//enhance the document text
+document.body.bgColor = "lightpink";
+
+document.getElementById('f2').style.color = "blue";
+document.getElementById('f3').style.color = "red";
+document.getElementById('f4').style.color = "purple";
+document.getElementById('f5').style.color = "green";
